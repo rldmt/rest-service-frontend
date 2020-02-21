f44e549d196a04d4922548622368440e70185bc5
rldmt <erialdo.meta@gmail.com>
2020-02-21 12:39:07 +0100

    show hotels functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'hotels', pathMatch: 'full' },
+  { path: 'hotels', component: HotelListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
+import { AddHotelComponent } from './components/add-hotel/add-hotel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelListComponent,
+    AddHotelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
